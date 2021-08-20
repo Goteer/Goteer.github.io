6a4835c32c1db6f72db1413028722adf9148b9a7
Goteer <joaquindiazchurria@gmail.com>
2021-08-20 19:38:30 -0300

    Better topbar

--- scripts/navigation_functions.js
+++ scripts/navigation_functions.js
@@ -1,6 +1,14 @@
-let links = { //Topbar links "array"
-    'Index': "/index.html",
-    '3D Demo': "/pages/threejs_demo/demo.html",
+let linksArray = { //Topbar links "array"
+    'Index': { 'Type': "/index.html" },
+    'Animation': { 'Type': "/index.html" },
+    'Three.js Demos': {
+        'Type': "Dropdown",
+        'Links': {
+            'Demo1': "/pages/threejs_demo/demo.html",
+            'Demo2': "/pages/threejs_demo/demo.html",
+            'Demo3': "/pages/threejs_demo/demo.html"
+        }
+    }
 };
 
 
@@ -8,6 +16,52 @@ let links = { //Topbar links "array"
 let getParams = {};
 loadGetParams();
 
+function appendLinks(links, linkListName) {
+    for (var [key, value] of Object.entries(links)) {
+        var link;
+        if (typeof value === 'object') {
+            switch (value.Type) {
+                case "Dropdown":
+                    var dropDiv = document.createElement("div");
+                    dropDiv.className = "menu-dropdown";
+                    document.getElementById(linkListName).appendChild(dropDiv)
+
+                    link = document.createElement("span");
+                    link.appendChild(document.createTextNode(key));
+                    link.title = key;
+                    link.id = key;
+                    link.className = "dropbtn"
+                    dropDiv.appendChild(link);
+                    var menuDiv = document.createElement("div");
+                    menuDiv.className = "menu-dropdown-content";
+                    dropDiv.appendChild(menuDiv)
+                    for (var [linkKey, linkValue] of Object.entries(value["Links"])) {
+                        link = document.createElement("a");
+                        link.appendChild(document.createTextNode(linkKey));
+                        link.title = linkKey;
+                        link.id = linkKey;
+                        link.href = linkValue;
+                        menuDiv.appendChild(link);
+                    }
+                    break;
+
+                default:
+                    link = document.createElement("a");
+                    link.appendChild(document.createTextNode(key));
+                    link.title = key;
+                    link.id = key;
+                    link.className = "dropbtn"
+                    link.href = value["Type"]; //This is so you can just input the URL directly instead of making yet another object within
+                    document.getElementById(linkListName).appendChild(link);
+                    break;
+            }
+        }
+
+
+
+
+    }
+}
 
 function loadTopbar(pageUrl) {
 
@@ -20,13 +74,7 @@ function loadTopbar(pageUrl) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("topbar_loader").innerHTML = this.responseText;
-            for (var [key, value] of Object.entries(links)) {
-                var link = document.createElement("a");
-                link.appendChild(document.createTextNode(key));
-                link.title = key;
-                link.href = value;
-                document.getElementById("link-list").appendChild(link);
-            }
+            appendLinks(linksArray, "link-list");
         } else {
             document.getElementById("topbar_loader").innerHTML = "There was a problem trying to get the page you requested. <button onclick='loadPage()'>Try reloading the page</button> or <button onclick='loadPage()'>Go to the home page</button>";
         }
