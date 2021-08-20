c785b1bc270a91da727b9e09c4b6b9460808c401
JoaquinChurria <joaquindiazchurria@gmail.com>
2021-08-19 21:03:04 -0300

    General Rework in progress

--- scripts/navigation_functions.js
+++ scripts/navigation_functions.js
@@ -1,32 +1,48 @@
+let links = { //Topbar links "array"
+    'Index': "/index.html",
+    '3D Demo': "/pages/threejs_demo/demo.html",
+};
+
+
+
 let getParams = {};
 loadGetParams();
 
 
-function loadPage(pageUrl){
+function loadTopbar(pageUrl) {
 
-  if (pageUrl == undefined){
-      pageUrl = "/pages/index_page.html";
-  }
+    if (pageUrl == undefined) {
+        pageUrl = "/resources/sections/topbar.html";
+    }
 
 
-  var xhttp = new XMLHttpRequest();
- xhttp.onreadystatechange = function() {
-   if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("main_div_frame").innerHTML = this.responseText;
-  }else{
-    document.getElementById("main_div_frame").innerHTML = "There was a problem trying to get the page you requested. <button onclick='loadPage()'>Try reloading the page</button> or <button onclick='loadPage()'>Go to the home page</button>";
-  }
- };
- xhttp.open("GET", pageUrl, true);
- xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("topbar_loader").innerHTML = this.responseText;
+            for (var [key, value] of Object.entries(links)) {
+                var link = document.createElement("a");
+                link.appendChild(document.createTextNode(key));
+                link.title = key;
+                link.href = value;
+                document.getElementById("link-list").appendChild(link);
+            }
+        } else {
+            document.getElementById("topbar_loader").innerHTML = "There was a problem trying to get the page you requested. <button onclick='loadPage()'>Try reloading the page</button> or <button onclick='loadPage()'>Go to the home page</button>";
+        }
+    };
+    xhttp.open("GET", pageUrl, true);
+    xhttp.send();
+
+
 }
 
-function loadGetParams(){
+function loadGetParams() {
 
-  var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               getParams[pair[0]] = pair[1];
-       }
-  }
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        getParams[pair[0]] = pair[1];
+    }
+}
