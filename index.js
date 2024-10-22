d181a8d3f3fe80104cb50a13fb20f1d64160e9e8
Goteer <joaquindiazchurria@gmail.com>
2024-10-22 20:49:49 -0300

    98.css added and modified

--- index.js
+++ index.js
@@ -8,31 +8,30 @@ function scrollBackground(){
         document.getElementById("background-layer").children[i].style.top = (document.body.scrollTop/10)-30;
     }
     document.getElementsByClassName("logo_bg")[0].style.top = (document.body.scrollTop/10)+300;
-    document.getElementsByClassName("stars")[0].style.top = (document.body.scrollTop/40);
+    //document.getElementsByClassName("stars")[0].style.top = (document.body.scrollTop/40);
         
     for (var i = 0; i < document.getElementsByClassName("main_container")[0].children.length; i++){
 
-        var elementTop = document.getElementsByClassName("main_container")[0].children[i].getBoundingClientRect().y;
-        var elementBottom = document.getElementsByClassName("main_container")[0].children[i].getBoundingClientRect().y+document.getElementsByClassName("main_container")[0].children[i].clientHeight;
-        var elementCenter = document.getElementsByClassName("main_container")[0].children[i].getBoundingClientRect().y+document.getElementsByClassName("main_container")[0].children[i].clientHeight/2;
-        
-        if (elementTop > window.innerHeight/2 && elementBottom > window.innerHeight/2){
-            var scale = 1-Math.abs(((elementTop/window.innerHeight)-0.5)/15);
-        }else{
-            if (elementTop <= window.innerHeight/2 && elementBottom >= window.innerHeight/2){
-                var scale = 1
+        if (document.getElementsByClassName("main_container")[0].children[i].className !== "bg_overlay_clipping"){ 
+
+            var elementTop = document.getElementsByClassName("main_container")[0].children[i].getBoundingClientRect().y;
+            var elementBottom = document.getElementsByClassName("main_container")[0].children[i].getBoundingClientRect().y+document.getElementsByClassName("main_container")[0].children[i].clientHeight;
+            var elementCenter = document.getElementsByClassName("main_container")[0].children[i].getBoundingClientRect().y+document.getElementsByClassName("main_container")[0].children[i].clientHeight/2;
+            
+            if (elementTop > window.innerHeight/2 && elementBottom > window.innerHeight/2){
+                var scale = 1-Math.abs(((elementTop/window.innerHeight)-0.5)/15);
             }else{
-                if (elementBottom < window.innerHeight/2){
-                    var scale = 1-Math.abs(((elementBottom/window.innerHeight)-0.5)/15);
+                if (elementTop <= window.innerHeight/2 && elementBottom >= window.innerHeight/2){
+                    var scale = 1
+                }else{
+                    if (elementBottom < window.innerHeight/2){
+                        var scale = 1-Math.abs(((elementBottom/window.innerHeight)-0.5)/15);
+                    }
                 }
             }
-        }
 
-       
-
-
-        document.getElementsByClassName("main_container")[0].children[i].style.transform = "scale("+scale+")" ;
-    }
+            document.getElementsByClassName("main_container")[0].children[i].style.transform = "scale("+scale+")" ;
+    }}
 
 }
 
